3c84e59919cee793e7e42ba548c354ce23d60eee
ConnorAU <15099385+ConnorAU@users.noreply.github.com>
2025-04-28 15:27:54 +1000

    Add mod.cpp Adding to git for use in remote build workflow

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name="Gun Game by ConnorAU";
+overview="The full collection of Gun Game missions created by ConnorAU.";
+tooltipOwned="Gun Game by ConnorAU";
+action="https://github.com/ConnorAU/A3GunGame";
+
+hideName=0;
+hidePicture=0;
